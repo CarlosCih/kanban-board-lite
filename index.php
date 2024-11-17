2f9f0e47140e29d63712eb341b0923c5cb61cbb1
Carlos <a20200043@utem.edu.mx>
2024-11-17 13:18:50 -0600

    diseño

--- index.php
+++ index.php
@@ -5,8 +5,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Inicio</title>
-    <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.min.css">
-    <link rel="stylesheet" href="assets/css/style.css">
+    <?php include 'includes/php/head.php'; ?>
+
 </head>
 
 <body>
